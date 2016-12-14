dd86d4bb8d8baaa984542fccd332e089cc05db08
管伟 <guananddu@qq.com>
2016-12-14 17:34:06 +0800

    fix write file

--- bin/tt-wb.js
+++ bin/tt-wb.js
@@ -9,14 +9,37 @@ const wbDir = __dirname;
 const argv  = process.argv;
 const workDir = process.cwd();
 
+// 检查是不是查看版本号
+if ( argv.length === 3 && argv[ 2 ] === '-V' ) {
+    let version = require( '../package.json' ).version;
+    console.log( `webpack-wrapper-common: ${version}` );
+    return;
+}
+
 // 检查是不是要初始化项目
 if ( argv.length === 3 && argv[ 2 ] === 'init' ) {
     console.log( 'Creating assets...' );
     let targetDir = path.resolve( __dirname, '..', 'assets/webpack_demo' );
     shell.cp( '-rf', `${targetDir}/*`, workDir );
+
     // copy .gitignore
-    shell.cp( '-rf', `${targetDir}/.gitignore`, workDir );
-    shell.cp( '-rf', `${targetDir}/output/.gitignore`, `${workDir}/output` );
+    // shell.cp( '-rf', `${targetDir}/.gitignore`, workDir );
+    // shell.cp( '-rf', `${targetDir}/output/.gitignore`, `${workDir}/output` );
+
+    // 直接写入文件
+    fs.writeFileSync( `${workDir}/.gitignore`, [
+        'node_modules',
+        'dev_modules',
+        'bower_components'
+    ].join( '\n' ) );
+
+    fs.writeFileSync( `${workDir}/output/.gitignore`, [
+        'page',
+        'static',
+        'template',
+        'resource'
+    ].join( '\n' ) );
+
     console.log( 'Assets created.' );
     return;
 }
