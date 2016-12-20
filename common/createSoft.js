10b2b46defe1b96a3df567e688d4dee9cba4605c
huangpei <huangpei@bytedance.com>
2016-12-20 14:50:31 +0800

    [webpack-wrapper-common]diff node_module

--- common/createSoft.js
+++ common/createSoft.js
@@ -4,9 +4,12 @@ const fs = require( 'fs' );
 const wbDir = __dirname;
 const workDir = process.cwd();
 
-const wbNodeModulesPath = path.resolve( wbDir, '../node_modules' );
-const workDirModulesPath = path.resolve( workDir, 'node_modules' );
+const wbNodeModulesPath = path.resolve( wbDir, '../node_modules/*' );
+const workDirModulesPath = path.resolve( workDir, 'node_modules/' );
 
 // create node_modules soft link
-! fs.existsSync( workDirModulesPath )
-    && cp.exec( `ln -s ${wbNodeModulesPath} ${workDirModulesPath}` );
+if (fs.existsSync( workDirModulesPath ) ) {
+    cp.exec( `ln -s ${wbNodeModulesPath} ${workDirModulesPath}` );
+} else {
+    cp.exec( `mkdir ${workDir}/node_modules && ln -s ${wbNodeModulesPath} ${workDirModulesPath}` )
+}
